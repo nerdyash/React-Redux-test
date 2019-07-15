32c4b6534bd79f50f0528a96a1d323f02dc75ca4
Yash Thakkar <nerdyash@yahoo.com>
2019-07-15 13:30:07 -0400

    Updates

---
+++ precache-manifest.53362fa19abc7f3820441bd8fa54a159.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2168b4bbfee48589a03c928869462034",
+    "url": "/React-Redux-test/index.html"
+  },
+  {
+    "revision": "364e08ed5d787c1bfd38",
+    "url": "/React-Redux-test/static/css/main.82e912ea.chunk.css"
+  },
+  {
+    "revision": "cbde24d8fe1fcde750ff",
+    "url": "/React-Redux-test/static/js/2.d5b57541.chunk.js"
+  },
+  {
+    "revision": "364e08ed5d787c1bfd38",
+    "url": "/React-Redux-test/static/js/main.d4467ca6.chunk.js"
+  },
+  {
+    "revision": "b8834d4481776ed4fe74",
+    "url": "/React-Redux-test/static/js/runtime~main.0451e83b.js"
+  }
+]);
